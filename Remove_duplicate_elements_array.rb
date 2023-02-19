67cc9897f9472bf894fcc317e20b2c83ee2c26d8
prabal <prabal5ghosh@gmail.com>
2023-02-20 00:50:07 +0530

    remove duplicate elements from an array using ruby

---
+++ Remove_duplicate_elements_array.rb
@@ -0,0 +1,18 @@
+# Ruby Array Exercises: Remove duplicate elements from a given array
+
+
+nums = [1, 2, 3, 4, 1, 2, 2, 3, 5, 6]
+print "Original array:\n"
+print nums
+print "\n Array with unique elements:\n"
+new_nums = nums.uniq
+print new_nums
+
+
+
+# output===
+
+# Original array:
+# [1, 2, 3, 4, 1, 2, 2, 3, 5, 6]
+#  Array with unique elements:
+# [1, 2, 3, 4, 5, 6]
